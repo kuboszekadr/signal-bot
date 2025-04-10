fbe8250d25ffdb08e404eae7ca549524c92c7969
Ubuntu <azureuser@signal-cli.21j1dzgvh1bedco2auaflzsp4f.frax.internal.cloudapp.net>
2025-04-10 07:37:51 +0000

    Added stream of raw envelopes coming from the stream

--- main.py
+++ main.py
@@ -9,12 +9,13 @@ from src.models.signal import *
 from src.models.db import local_session, Chat
 from src.handlers import ReceiveProcess, SendMessage
 from src.logger import logger
+from src.config import app_config
 
 from src.agent.agent import invoke
 
 def save_envelope(envelope: Envelope):
     file_path = os.path.join(
-        './data',
+        app_config.envelopes_path,
         envelope.chat_id(),
         dt.now().strftime("%Y-%m"),
         f'{dt.now().strftime("%Y-%m-%d")}.jsonl'
@@ -34,12 +35,28 @@ def save_envelope(envelope: Envelope):
     session = local_session()
     session.add(message)
     session.commit()
+    session.close()
+
+def dump_stream(line):
+    folder_name = dt.now().strftime("%Y-%m-%d")
+    file_path = os.path.join(
+        app_config.stream_path,
+        f'{folder_name}.jsonl'
+    )
+
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
+    with jsonlines.open(file_path, 'a') as writer:
+        writer.write(line)
 
 def monitor_incoming_msgs():
     receive_process = ReceiveProcess().start_receive_process()
     while True:
         for line in iter(receive_process.stdout.readline, ''):
             logger.info(line)
+            dump_stream(line)
+
             try:
                 envelope_json = json.loads(line).get('envelope')
                 envelope = Envelope(**envelope_json)
@@ -50,8 +67,9 @@ def monitor_incoming_msgs():
             message = envelope.get_message()
             if message is None:
                 continue
-
+            
             save_envelope(envelope)
+            
             if not message.is_ai_call():
                 continue
 
@@ -59,6 +77,7 @@ def monitor_incoming_msgs():
             receive_process.kill()
 
             # signal cli limitation - you can only have one process running at a time
+            # TODO: maybe this can be fixed by using d-bus?
             llm_input = message.message.replace("@bot", "").strip()
             context = ''
             if message.quote is not None:
@@ -88,3 +107,5 @@ def monitor_incoming_msgs():
                 params=args
             )
             receive_process = ReceiveProcess().start_receive_process()
+
+monitor_incoming_msgs()
